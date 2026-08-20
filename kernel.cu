b202ccbb8b8c3f3c399319388886fe26c398af3c
Givy120 <givikap120@gmail.com>
2026-08-20 20:52:24 +0200

    Initial commit

---
+++ kernel.cu
@@ -0,0 +1,57 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <cstdlib>
+#include <iostream>
+
+void cudaCheck(cudaError_t call)
+{                                
+    if (call != cudaSuccess)
+    {                                                         
+        std::cerr << "CUDA error: "                           
+                  << cudaGetErrorString(call)
+                  << '\n';                                    
+        std::exit(1);                                         
+    }                                                         
+}
+
+__global__ void helloKernel()
+{
+    printf(
+        "Hello from GPU thread %d\n",
+        threadIdx.x
+    );
+}
+
+
+int main()
+{
+    cudaDeviceProp properties{};
+
+    cudaCheck(cudaGetDeviceProperties(&properties, 0));
+
+    std::cout
+        << "GPU: "
+        << properties.name
+        << '\n';
+
+    std::cout
+        << "Compute capability: "
+        << properties.major
+        << '.'
+        << properties.minor
+        << '\n';
+
+    std::cout
+        << "SM count: "
+        << properties.multiProcessorCount
+        << "\n\n";
+
+    helloKernel<<<1, 4>>>();
+
+    cudaCheck(cudaGetLastError());
+    cudaCheck(cudaDeviceSynchronize());
+
+    return 0;
+}
